df4f6baba2c652ea8487df71b35d826b710cc22b
Ariunsanaa <ariunsanaa.9161@gmail.com>
2020-08-01 15:27:25 -0700

    intro done

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const postcss_plugins = [
+  require("postcss-import"),
+  require("postcss-mixins"),
+  require("postcss-simple-vars"), // css dotor variable zarlaj bolno
+  require("postcss-hexrgba"), // rgba() dotor ungiig variable-eer duudaj bolno
+  require("postcss-nested"),
+  require("autoprefixer"),
+];
+module.exports = {
+  entry: "./app/assets/scripts/main.js",
+  output: {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app"),
+  },
+
+  devServer: {
+    before: function (app, server) {
+      server._watch("./app/**/*.html");
+    },
+    contentBase: path.join(__dirname, "app"),
+
+    hot: true,
+    port: 8080,
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          { loader: "postcss-loader", options: { plugins: postcss_plugins } },
+        ],
+      },
+    ],
+  },
+};
