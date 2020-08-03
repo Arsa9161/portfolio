94661ed49d7dc745b035d7d7e54903f05e98e17c
Ariunsanaa <ariunsanaa.9161@gmail.com>
2020-08-03 14:30:06 -0700

    intro zagvar zaslaa

--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,9 @@ module.exports = {
       server._watch("./app/**/*.html");
     },
     contentBase: path.join(__dirname, "app"),
-
     hot: true,
     port: 8080,
+    host: "0.0.0.0",
   },
   mode: "development",
   module: {
